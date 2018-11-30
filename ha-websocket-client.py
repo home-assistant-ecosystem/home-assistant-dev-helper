e26a208d9848237a1fc8e88bcc709dfb0745056d
Fabian Affolter <fabian@affolter-engineering.ch>
2018-11-30 19:16:23 +0100

    Update WS example for new Auth system

--- ha-websocket-client.py
+++ ha-websocket-client.py
@@ -1,25 +1,35 @@
 #!/usr/bin/python3
 #
-# Copyright (c) 2017, Fabian Affolter <fabian@affolter-engineering.ch>
+# Copyright (c) 2017-2018, Fabian Affolter <fabian@affolter-engineering.ch>
 # Released under the ASL 2.0 license. See LICENSE.md file for details.
 #
 import asyncio
-import asyncws
 import json
 
-@asyncio.coroutine
-def echo():
-    websocket = yield from asyncws.connect('ws://localhost:8123/api/websocket')
+import asyncws
 
-    yield from websocket.send(json.dumps(
-        {'id': 1, 'type': 'subscribe_events', 'event_type': 'state_changed'}))
+ACCESS_TOKEN = 'ABCDE'
+
+
+async def main():
+    """Simple echo WebSocket client for Home Assistant."""
+    websocket = await asyncws.connect('ws://localhost:8123/api/websocket')
+
+    await websocket.send(json.dumps(
+        {'type': 'auth',
+         'access_token': ACCESS_TOKEN}
+    ))
+
+    await websocket.send(json.dumps(
+        {'id': 1, 'type': 'subscribe_events', 'event_type': 'state_changed'}
+    ))
 
     while True:
-        message = yield from websocket.recv()
+        message = await websocket.recv()
         if message is None:
             break
         print (message)
 
-asyncio.get_event_loop().run_until_complete(echo())
-asyncio.get_event_loop().close()
-
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
+loop.close()
