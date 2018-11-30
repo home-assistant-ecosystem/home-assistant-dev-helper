a6e5346589dd231cf2d800d264bb25d32beda556
Fabian Affolter <fabian@affolter-engineering.ch>
2018-11-30 19:21:53 +0100

    Minor changes

--- ha-websocket-aiohttp-client.py
+++ ha-websocket-aiohttp-client.py
@@ -4,20 +4,27 @@
 # Released under the ASL 2.0 license. See LICENSE.md file for details.
 #
 import asyncio
-import aiohttp
 import json
 
-async def echo():
+import aiohttp
 
-    async with aiohttp.ClientSession().ws_connect('http://localhost:8123/api/websocket') as ws:
+ACCESS_TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiI3ZDg2Mzc3NzIwYjQ0M2YyOWI2MzE2ZTdmMjI3Njc0OCIsImlhdCI6MTU0MzYwMTY1OCwiZXhwIjoxODU4OTYxNjU4fQ.uSatzdHOC-ozC9OnI0pUk63Mtuawy7bauRG6k-swP9g'
+
+
+async def main():
+    """Simple WebSocket client for Home Assistant."""
+    async with aiohttp.ClientSession().ws_connect(
+            'http://localhost:8123/api/websocket') as ws:
             
             await ws.send_str(json.dumps(
                 {'type': 'auth', 
-                  'access_token': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiIzZTI0MWZiNGQzN2M0YjA5YTZiMzBmNzZiZDkzMDRjNiIsImlhdCI6MTU0MzU4NDM1OSwiZXhwIjoxODU4OTQ0MzU5fQ.fcB9Rb7sQ3Z1E1c9jRLM8DZHrmulPVVNtdJaxvW7wsg'}
-                  ))
+                 'access_token': ACCESS_TOKEN}
+            ))
         
             await ws.send_str(json.dumps(
-            {'id': 1, 'type': 'subscribe_events', 'event_type': 'state_changed'}))
+                {'id': 1, 'type': 'subscribe_events',
+                 'event_type': 'state_changed'}
+            ))
             
             while True:
                 msg = await ws.receive()
@@ -28,5 +35,5 @@ async def echo():
                     break
                     
 loop = asyncio.get_event_loop()  
-loop.run_until_complete(echo())  
+loop.run_until_complete(main())
 loop.close()  
